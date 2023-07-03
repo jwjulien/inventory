aae1d5d3174e9583560963887c7d5d9181d99728
jared <jaredjulien@exsystems.net>
2023-07-03 08:38:31 -0400

    cleaned up abandoned sqlalchemy bits from category model

--- inventory/model/categories.py
+++ inventory/model/categories.py
@@ -22,20 +22,16 @@
 # ======================================================================================================================
 # Import Statements
 # ----------------------------------------------------------------------------------------------------------------------
-import collections
-from datetime import datetime
-from typing import List, Optional
-
-# from sqlalchemy import ForeignKey, select
-# from sqlalchemy.orm import relationship, backref, Mapped, mapped_column
-# from sqlalchemy.ext.hybrid import hybrid_property
-# from sqlalchemy import Column, Integer, String, DateTime
-from peewee import CharField, DateTimeField, ForeignKeyField, PrimaryKeyField
+from peewee import CharField, ForeignKeyField
 
 from inventory.model.base import BaseModel
 
 
 
+
+# ======================================================================================================================
+# Category Model
+# ----------------------------------------------------------------------------------------------------------------------
 class Category(BaseModel):
     class Meta:
         table_name = 'categories'
@@ -58,130 +54,6 @@ class Category(BaseModel):
             return self.parent.inherited_designator
         # And if all else fails, None indicates that no designator was assigned.
         return None
-
-
-    def child_number(self) -> int:
-        """Return the index of this category relative to it's sibling assigned to the same parent."""
-        if self.parent is None:
-            return None
-        siblings = sorted(self.parent.children, key=lambda child: child.title)
-        return siblings.index(self)
-
-
-
-
-# ======================================================================================================================
-# Part Categories Table
-# ----------------------------------------------------------------------------------------------------------------------
-# class PartCategory(BaseModel):
-#     __tablename__ = 'part_categories'
-
-#     # Columns
-#     id: Mapped[int] = mapped_column(init=False, primary_key=True)
-#     parent_id: Mapped[int] = mapped_column(ForeignKey('part_categories.id'))
-#     default_designator: Mapped[Optional[str]] = mapped_column(String(10))
-#     title: Mapped[str] = mapped_column(String(50))
-#     created_on: Mapped[datetime]
-#     # modified_on: Mapped[Optional[datetime]]
-
-#     # Relationships
-#     children = relationship('PartCategory',
-#         cascade="all",
-#         backref=backref("parent", remote_side='PartCategory.id'),
-#         order_by='PartCategory.title',
-#     )
-#     parts = relationship('Part', backref='category', order_by='Part.value')
-
-
-# # ----------------------------------------------------------------------------------------------------------------------
-#     # Properties
-#     @hybrid_property
-#     def invalid(self) -> bool:
-#         return not self.title
-
-
-#     @hybrid_property
-#     def inherited(self) -> bool:
-#         return self.default_designator is None
-
-
-#     @hybrid_property
-#     def all_parts(self) -> List:
-#         """Get a list of all of the parts under this category including the parts assigned to the children."""
-#         def recurse(category):
-#             parts = []
-#             parts.extend(category.parts)
-#             for child in category.children:
-#                 parts.extend(recurse(child))
-#             return parts
-#         return recurse(self)
-
-
-#     @hybrid_property
-#     def is_leaf(self) -> bool:
-#         return len(self.children) == 0
-
-
-#     @hybrid_property
-#     def depth(self) -> int:
-#         if not self.parent:
-#             return 0
-#         else:
-#             return self.parent.depth + 1
-
-
-#     @hybrid_property
-#     def full_title(self) -> str:
-#         if self.parent:
-#             return self.parent.full_title + '.' + self.title
-#         return self.title
-
-
-#     @hybrid_property
-#     def designator(self) -> str:
-#         if self.default_designator:
-#             return self.default_designator
-#         elif self.parent:
-#             return self.parent.designator
-#         return None
-
-#     @designator.setter
-#     def designator(self, value):
-#         self.default_designator = value
-
-
-# # ----------------------------------------------------------------------------------------------------------------------
-#     # Static Methods
-#     @staticmethod
-#     def GetById(session, id):
-#         return session.query(PartCategory).filter_by(id=id).first()
-
-
-#     @staticmethod
-#     def GetAll(session):
-#         return session.query(PartCategory).all()
-
-
-#     @staticmethod
-#     def GetRoots(session):
-#         return list(session.execute(select(PartCategory).filter_by(parent_id='').order_by(PartCategory.title)).scalars())
-
-
-#     @staticmethod
-#     def GetDict(session):
-#         categories = collections.OrderedDict()
-#         def addItems(nodes):
-#             for node in nodes:
-#                 categories[node.id] = node.full_title
-#                 addItems(node.children)
-#         roots = PartCategory.GetRoots(session)
-#         addItems(roots)
-#         return categories
-
-
-#     @staticmethod
-#     def GetChildren(session, parent_id):
-#         return session.query(PartCategory).filter_by(parent_id=parent_id).all()
 
 
 
